a4b336fd7575009ed82a1071acb44e6f9c574bc3
Michael J Cerce <137245672+MichaelJCerce@users.noreply.github.com>
2023-07-26 09:09:08 -0400

    Add return <section> and styles to AvailableMeals

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,3 +1,45 @@
-const AvailableMeals = () => {};
+import classes from "./AvailableMeals.module.css";
+
+const DUMMY_MEALS = [
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
+];
+const AvailableMeals = () => {
+  return (
+    <section className={classes.meals}>
+      <ul>
+        {DUMMY_MEALS.map((meal) => (
+          <li key={meal.id}>
+            <div>{meal.name}</div>
+            <div>{meal.description}</div>
+            <div>{meal.price}</div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
 
 export default AvailableMeals;
