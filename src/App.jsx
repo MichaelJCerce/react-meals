76cc04d7a0d4f219ec86bf25ee07a63b281b5961
Michael J Cerce <137245672+MichaelJCerce@users.noreply.github.com>
2023-07-26 09:34:40 -0400

    Create Card.jsx and .css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 function App() {
-  return <Header></Header>;
+  return (
+    <>
+      <Header></Header>
+      <main style={{clear: "both"}}>
+        <Meals />
+      </main>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/UI/Card.jsx
@@ -0,0 +1,6 @@
+import classes from "./Card.module.css";
+const Card = (props) => {
+  return <div></div>;
+};
+
+export default Card;
