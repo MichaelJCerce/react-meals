69c03ad5d27556c5771dd894325c92b13e471df9
Michael J Cerce <137245672+MichaelJCerce@users.noreply.github.com>
2023-07-26 12:14:20 -0400

    Create Modal .jsx and .css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 function App() {
   return (
     <>
       <Header></Header>
       <main style={{clear: "both"}}>
         <Meals />
+        <Cart></Cart>
       </main>
     </>
   );
