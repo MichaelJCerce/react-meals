ae83afc35b05bd8de1523f2115c5acf22e3a2c07
Michael J Cerce <137245672+MichaelJCerce@users.noreply.github.com>
2023-07-26 14:30:01 -0400

    Add context to header cart badge

--- src/components/Layout/HeaderCardButton.jsx
+++ src/components/Layout/HeaderCardButton.jsx
@@ -1,14 +1,23 @@
+import { useContext } from "react";
+import { CartContext } from "../../context/CartProvider";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCardButton.module.css";
 
 const HeaderCardButton = (props) => {
+  const ctx = useContext(CartContext);
+
+  const numberOfCartItems = ctx.items.reduce(
+    (sum, item) => sum + item.amount,
+    0
+  );
+
   return (
     <button onClick={props.onClick} className={classes.button}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
--- src/context/CartProvider.jsx
+++ src/context/CartProvider.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const CartContext = React.createContext({
+export const CartContext = React.createContext({
   items: [],
   totalAmount: 0,
   addItem: () => {},
