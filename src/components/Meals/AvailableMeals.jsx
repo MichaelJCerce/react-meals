b013ff8d6f567b52772391930e9c256930dbadbc
Michael J Cerce <137245672+MichaelJCerce@users.noreply.github.com>
2023-08-03 15:55:57 -0400

    Pull list of meals from Firebase database

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,39 +1,64 @@
 import Card from "../UI/Card";
 import Meal from "./Meal";
 import classes from "./AvailableMeals.module.css";
-
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+import { useEffect, useState } from "react";
 
 const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [httpError, setHttpError] = useState(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://react-http-3eaf7-default-rtdb.firebaseio.com/meals.json"
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+      setHttpError(false);
+      const data = await response.json();
+      // console.log(
+      //   "entries",
+      //   Object.entries(data).reduce(
+      //     (meals, [id, meal]) => [...meals, { id, ...meal }],
+      //     []
+      //   )
+      // );
+      const loadedMeals = [];
+      for (let key in data) {
+        loadedMeals.push({ id: key, ...data[key] });
+      }
+      setIsLoading(false);
+      setMeals(loadedMeals);
+    };
+    fetchMeals().catch((error) => {
+      setHttpError(error.message);
+      setIsLoading(false);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading... </p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section>
+        <p className={classes.MealsError}>{httpError}</p>
+      </section>
+    );
+  }
+
   return (
     <Card className={classes.meals}>
       <ul>
-        {DUMMY_MEALS.map((meal) => (
+        {meals.map((meal) => (
           <Meal key={meal.id} {...meal} />
         ))}
       </ul>
